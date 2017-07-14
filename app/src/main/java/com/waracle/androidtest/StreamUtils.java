09bbab8057267189f8a4129c6cb37c1fb08d734f
Ilian Georgiev <georgiev.ilian@gmail.com>
2017-07-14 15:49:34 +0300

    Fix warnings.

--- app/src/main/java/com/waracle/androidtest/StreamUtils.java
+++ app/src/main/java/com/waracle/androidtest/StreamUtils.java
@@ -9,12 +9,13 @@ import java.util.ArrayList;
 
 /**
  * Created by Riad on 20/05/2015.
+ *
  */
-public class StreamUtils {
+class StreamUtils {
     private static final String TAG = StreamUtils.class.getSimpleName();
 
     // Can you see what's wrong with this???
-    public static byte[] readUnknownFully(InputStream stream) throws IOException {
+    static byte[] readUnknownFully(InputStream stream) throws IOException {
         // Read in stream of bytes
         ArrayList<Byte> data = new ArrayList<>();
         while (true) {
@@ -35,7 +36,7 @@ public class StreamUtils {
         return bytes;
     }
 
-    public static void close(Closeable closeable) {
+    static void close(Closeable closeable) {
         if (closeable != null) {
             try {
                 closeable.close();
